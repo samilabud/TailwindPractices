571428c95aecff58cb18cccfc942c2349da6f419
samilabud <samilabud@gmail.com>
2023-09-26 11:29:55 -0400

    First commit - custom them, using apply and added rules to tailwind tags works

---
+++ Tutorial/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="flex">
+
+      <div className="card">
+          Test card
+      </div>
+      <section className="h-screen bg-twitter-blue flex items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fugit sequi odit natus inventore unde cumque ut laudantium nemo obcaecati deserunt velit dolorem quas, illum quos totam quia aspernatur! Similique?</p>
+      </section>
+
+    </div>
+  )
+}
+
+export default App;
